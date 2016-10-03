c7590aa265985c02fca080254003dece6ed5ee06
elicorrales <e_corrales2003@yahoo.com>
2016-10-03 10:33:56 -0400

    .

---
+++ 1-ws-rest-p1/src/main/java/com/eli/calc/shape/service/ws/parms/QueueCalculationParms.java
@@ -0,0 +1,38 @@
+
+package com.eli.calc.shape.service.ws.parms;
+
+import com.eli.calc.shape.model.CalcType;
+import com.eli.calc.shape.model.ShapeName;
+
+
+public class QueueCalculationParms {
+
+    protected ShapeName shapeName;
+    protected CalcType calcType;
+    protected double dimension;
+
+    public ShapeName getShapeName() {
+        return shapeName;
+    }
+
+    public void setShapeName(ShapeName value) {
+        this.shapeName = value;
+    }
+
+    public CalcType getCalcType() {
+        return calcType;
+    }
+
+    public void setCalcType(CalcType value) {
+        this.calcType = value;
+    }
+
+    public double getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(double value) {
+        this.dimension = value;
+    }
+
+}
